f4b6862e1261a67f699345a33dc32f55eb4d6a4f
ThiagoAndo <ando.thiago@gmail.com>
2024-08-15 16:45:19 +0100

    .ico into app

--- app/layout.js
+++ app/layout.js
@@ -12,9 +12,9 @@ import ReduxProvider from "@/store/provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio of Abu Said - Software Developer",
+  title: "Portfolio of Thiago Freitas- Software Developer",
   description:
-    "This is the portfolio of Abu Said. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+    "This is the portfolio of Thiago. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
 };
 
 export default function RootLayout({ children }) {
