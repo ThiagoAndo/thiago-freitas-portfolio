19956a8a944d4f320f4f6bd8f6e1d54fda896012
ThiagoAndo <ando.thiago@gmail.com>
2024-08-16 12:45:11 +0100

    add custom hook

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -23,12 +23,12 @@ function HeroSection() {
       />
 
       <div className="order-2 lg:order-1 flex flex-col items-start justify-center pb-20 md:pb-10 lg:pt-10">
-        <p className="text-2xl  md:text-5xl text-neutral-700 !leading-snug text-center mx-auto font-[900] text-white mt-4">
+        <p className="text-2xl my-2 md:text-5xl text-neutral-700 !leading-snug text-center mx-auto font-[900] text-white mt-4">
           <span className={`text-[#00adf4] t-anime`}>
             <DecoderText text={"THIAGO FREITAS,"} delay={500} />
           </span>
         </p>
-        <h1 className="text-2xl  md:text-6xl text-neutral-700 !leading-snug text-center mx-auto font-[900] text-white mt-6">
+        <h1 className="text-2xl  my-2  md:text-6xl text-neutral-700 !leading-snug text-center mx-auto font-[900] text-white mt-6">
           <Designation />
         </h1>
         <div className="my-12 flex items-center gap-5 m-auto">
