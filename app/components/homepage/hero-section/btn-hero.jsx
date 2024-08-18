6798f6e8975528a834f479b736e98a1e224d61e0
ThiagoAndo <ando.thiago@gmail.com>
2024-08-18 21:25:33 +0100

    Add btn.css

--- app/components/homepage/hero-section/btn-hero.jsx
+++ app/components/homepage/hero-section/btn-hero.jsx
@@ -1,12 +1,10 @@
 import Link from "next/link";
+import './btn.css'
 
 export default function Btns({ children }) {
   return (
     <Link href="#contact">
-      <button
-        id="anime-cont"
-        className="px-3 text-xs md:px-8 py-3 md:py-4 bg-[#0d1224] rounded-full border-none text-center md:text-sm font-medium uppercase tracking-wider text-[#ffff] no-underline transition-all duration-200 ease-out  md:font-semibold flex items-center gap-1"
-      >
+      <button className="anime-cont px-3 text-xs md:px-8 py-3 md:py-4 bg-[#0d1224] rounded-full border-none text-center md:text-sm font-medium uppercase tracking-wider text-[#ffff] no-underline transition-all duration-200 ease-out  md:font-semibold flex items-center gap-1">
         {children}
       </button>
     </Link>
